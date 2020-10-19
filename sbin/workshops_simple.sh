888bb223ce178c0165cb63b583a4431d1c257be1
Dan Burton <danburton.email@gmail.com>
2020-10-19 13:43:51 -0400

    algo tests for workshops

--- sbin/workshops_simple.sh
+++ sbin/workshops_simple.sh
@@ -5,7 +5,13 @@ for WORKSHOP in hash-lock relay trust-fund ; do
   echo
   echo "============ workshop-$WORKSHOP [begin] ==============="
   echo
+
+  # Works w/o Makefile (ETH only)
   sbin/simple_test.sh "workshop-$WORKSHOP"
+
+  # Works w/o Makefile (ALGO only)
+  sbin/simple_test_ALGO.sh "workshop-$WORKSHOP"
+
   echo
   echo "============ workshop-$WORKSHOP [end] ================="
   echo
