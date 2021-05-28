0a1d9f05fe5db6e18f26ce2a9fb58cc3bf4f2156
Dan Burton <danburton.email@gmail.com>
2021-05-28 10:33:16 -0400

    not shallow clone b/c it ruins the git diff checks

--- sbin/setup.sh
+++ sbin/setup.sh
@@ -3,7 +3,7 @@ set -ex
 
 cd /tmp || exit 1
 rm -rf reach-lang
-git clone --depth=1 https://github.com/reach-sh/reach-lang
+git clone https://github.com/reach-sh/reach-lang
 
 # https://docs.reach.sh/tut-0.html
 curl https://raw.githubusercontent.com/reach-sh/reach-lang/master/reach -o reach ; chmod +x reach
